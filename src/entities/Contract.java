3fe99b3edbaa7f76721b0e6868298e5fc065ebcb
Gabriel Silva Torres <gabrielst328@gmail.com>
2023-12-01 16:06:17 -0300

    Update Contract

--- src/entities/Contract.java
+++ src/entities/Contract.java
@@ -1,5 +1,29 @@
 package entities;
 
+import java.util.Date;
+
 public class Contract {
+	
+	private Integer number;
+	private Date date;
+	private Double totalValue;
+	
+	public Contract(Integer number, Date date, Double totalValue) {
+		this.number = number;
+		this.date = date;
+		this.totalValue = totalValue;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public Double getTotalValue() {
+		return totalValue;
+	}
 
 }
